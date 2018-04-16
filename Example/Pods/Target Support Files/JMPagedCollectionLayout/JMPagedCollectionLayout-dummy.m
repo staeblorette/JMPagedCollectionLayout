d281d448c00abc9b7903977668869d9eaaaf3729
Martin Stähler <martin.staehler@gmail.com>
2018-04-16 13:42:14 +0200

    Create bare project setup via pods

---
+++ Example/Pods/Target Support Files/JMPagedCollectionLayout/JMPagedCollectionLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JMPagedCollectionLayout : NSObject
+@end
+@implementation PodsDummy_JMPagedCollectionLayout
+@end
---
+++ Example/Pods/Target Support Files/Pods-JMPagedCollectionLayout_Example/Pods-JMPagedCollectionLayout_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JMPagedCollectionLayout_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JMPagedCollectionLayout_Example
+@end
